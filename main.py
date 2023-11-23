fbd955d26e0e49e954a39a3f263fb11852338a45
Nathan Garcia <117130078+nathangarciaf@users.noreply.github.com>
2023-11-23 13:21:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import sys
 from bot import run_bot_terminal
 
 def main():
-    run_bot_terminal("Oi")
+    n = len(sys.argv)
+    resp=""
+    for i in range(1,n):
+        resp += sys.argv[i] + " "
+    print(resp)
+    run_bot_terminal(resp)
     
 if __name__ == "__main__":
     main()
